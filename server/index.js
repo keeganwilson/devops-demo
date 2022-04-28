b7d558ed4e13d597d282b51e648751bae6ae48c9
Keegan Wilson <keegan.wilson@gmail.com>
2022-04-28 12:21:56 -0600

    fixes

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (res, req) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 const port = process.env.PORT || 4545
 
-app.listen(4545, () => console.log(`Up and running on port ${port}`));
+app.listen(4545, () => {console.log(`Up and running on port ${port}`)});
