b43cb93ebcc863b0eed984b610c6d069eaa640bf
Keegan Wilson <keegan.wilson@gmail.com>
2022-04-28 15:07:55 -0600

    added post

--- server/index.js
+++ server/index.js
@@ -11,16 +11,31 @@ var rollbar = new Rollbar({
   captureUnhandledRejections: true,
 })
 
-// record a generic message and send it to Rollbar
-rollbar.log('Hello world!')
+let students = [];
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-try {
-    app.get('/', fakeFunc())
-}   catch(error) {console.error(error)};
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Keegan', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+
+})
 
 const port = process.env.PORT || 4545
 
