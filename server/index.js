1d5ad4562fc6c9897cb6b85169011f8eb2de3bc0
Keegan Wilson <keegan.wilson@gmail.com>
2022-04-28 15:17:16 -0600

    error removal

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.post('/api/student', (req, res)=>{
     let {name} = req.body
     name = name.trim()
 
-    const index = students.findIndex(studentName=> studentName === name)
+    const index = students.findIndex(studentName => studentName === name)
 
     if(index === -1 && name !== ''){
         students.push(name)
@@ -29,10 +29,10 @@ app.post('/api/student', (req, res)=>{
         res.status(200).send(students)
     } else if (name === ''){
         rollbar.error('No name given')
-        res.status(400).send('must provide a name.')
+        res.status(400).send('provide a name stupid')
     } else {
-        rollbar.error('student already exists')
-        res.status(400).send('that student already exists')
+        rollbar.error('student already added')
+        res.status(400).send('you already added that one')
     }
 
 })
